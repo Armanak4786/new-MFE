7799a8ef406a63dfdc31dd08a0a673d943c255be
Armanak4786 <arman.khan@aurionpro.com>
2026-01-30 11:59:41 +0530

    add favicon

--- projects/admin/src/app/admin.module.ts
+++ projects/admin/src/app/admin.module.ts
@@ -1,13 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin.component';
+import { LayoutModule } from './layout/layout.module';
+import { AuroUiFrameWork } from 'auro-ui';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'dashboard',
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+        data: { breadcrumb: 'Dashboard' },
+      },
+    ]
+  }
+];
 
 @NgModule({
-    declarations: [],
-    imports: [
-        CommonModule,
-        RouterModule.forChild([])
-    ]
+  declarations: [AdminComponent],
+  imports: [
+    CommonModule,
+    LayoutModule,
+    AuroUiFrameWork,
+    RouterModule.forChild(routes)
+  ]
 })
 export class AdminModule { }
-
