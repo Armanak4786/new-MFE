05fae493c3a7f1836a081820074ee32b589e407f
Armanak4786 <arman.khan@aurionpro.com>
2026-02-02 11:52:08 +0530

    add login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthenticationService, DataService } from 'auro-ui';
+import { AuthenticationService } from 'auro-ui';
 
 @Component({
     selector: 'app-login',
@@ -11,49 +10,13 @@ import { AuthenticationService, DataService } from 'auro-ui';
 export class LoginComponent {
     pageType: string = "login";
 
-    valCheck: string[] = ["remember"];
-    password!: string;
-    hidePassword: boolean = true;
-    loginForm: FormGroup;
-
     constructor(
         private authService: AuthenticationService,
-        private fb: FormBuilder,
-        private dataSvc: DataService,
-        private router: Router
     ) { }
 
-    ngOnInit(): void { 
-        this.loginForm = this.fb.group({
-            username: ["", [Validators.required]],
-            password: ["", [Validators.required]],
-            remember: [false, [Validators.requiredTrue]],
-          });
-    }
+    ngOnInit(): void { }
 
     loginWithOidc() {
         this.authService.oidcLogin();
     }
-
-    basicAuthentication() {
-        if (
-          !this.loginForm.controls["username"].value ||
-          !this.loginForm.controls["password"].value
-        ) {
-          this.loginForm.markAllAsTouched();
-          return;
-        }
-    
-        this.dataSvc
-          .getAccessToken(
-            "",
-            this.loginForm.controls["username"].value,
-            this.loginForm.controls["password"].value
-          )
-          .subscribe((data: any) => {
-            sessionStorage.setItem("accessToken", data?.access_token || null);
-            sessionStorage.setItem("refreshToken", data?.refresh_token || null);
-            this.router.navigate(["/landing"]);
-          });
-      }
 }
